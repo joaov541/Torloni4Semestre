801c513738728c78c5598f52d7a74c7c52afbebf
JOÃO VITOR DO NASCIMENTO SANTOS <54198904880@info.local>
2026-07-30 16:30:09 -0300

    Adicionado a Api local

--- reactlist/src/App.jsx
+++ reactlist/src/App.jsx
@@ -1,120 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import editIcon from "./assets/Vector (1).svg"
+import trashIcon from "./assets/Vector.svg"
 import './App.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // states e variáveis
+  //funções e effects
+
+  //criar um state chamado
+  //tasklist como iniciando com um array de 
+  //objetos [{xpto: abc, iii: uuu}]
+  //ja preenchido com 4 tarefas
+
+  //em seguida fazer um map e gerar os cards
+  //com todas as tarefas
+
+  const [tasklist, setTaskList] = useState([])
+
+  //CRUD 
+
+  //Read (Get)
+  const getTask = async () => {
+    //chamar a api
+    try {
+      const APIreturn = await axios.get("http://localhost:3000/taskpoin")
+      const bodyAPI = await APIreturn.data 
+
+      //e armazenar os dados no state
+      setTaskList(bodyAPI)
+    } catch (error) {
+      
+    }
+  }
+
+  //Create (Post)
+  const createTask = () => {}
+
+  //Uptade (Put/Patch)
+  const putTask = () => {}
+
+  //Delete (Delete)
+  const deleteTask = () => {}
+
+  //roda na montagem do componente - ciclo de vida dos componentes react
+  useEffect(() => {
+    getTask()
+  }, [])
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
+      <header className='header-section'>
+        <h1 className='header-section__title'>React List</h1>
+      </header>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <main className='body-section'>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        <form className='cad-task'>
+          <input className='cad-task__entry' type="text" placeholder='Adicione uma tarefa' />
+          <button className='cad-task__btn-confirm'>Adicionar</button>
+        </form>
+
+        <section className='cardlist'>
+
+          {
+            tasklist.map(task => {
+              return (
+          <article className="cardtask" key={task.id}>
+            <p className="cardtask__task-text">{task.descricao}</p>
+
+            <div className="cardtask__icon-box">
+              <div className="cardlist__icon">
+                <img src={editIcon} alt="" />
+              </div>
+              <div className="cardlist__icon">
+                <img src={trashIcon} alt="" />
+              </div>
+            </div>
+
+          </article>
+              )
+            })
+          }
+
+
+        </section>
+
+      </main>
+
+      <footer className='footer-section'>
+        <p className='footer-section__right-text'>2026 React List - Todos os direitos reservados</p>
+      </footer>
+
     </>
   )
 }
